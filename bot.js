db7bd38cb450807fbdcf886d2250a145f314182f
faracostakanan-art <faracostakanan@gmail.com>
2026-03-14 12:39:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-const token = const token = process.env.BOT_TOKEN;
+const token = process.env.BOT_TOKEN;
 const API_URL = "https://felina-backend-production.up.railway.app";
 const bot = new TelegramBot(token, { polling: true });
 
